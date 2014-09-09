903b1277eda6b42e9d415fa271db6204376d4c32
Sauli Tähkäpää <sauli@vaadin.com>
2014-09-09 14:14:46 +0300

    Added a install script.

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Saulis/vaadin-idea-workspace/archive/master.zip
+unzip -o master.zip
+cp -R vaadin-idea-workspace-master/ .
+rm -rf vaadin-idea-workspace-master
+rm master.zip
