589dc796ecf2cf9770fa119a08f210b4140b0f57
Sauli Tähkäpää <sauli@vaadin.com>
2014-11-27 09:52:33 +0200

    Removed template files.

--- install.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/Saulis/vaadin-idea-workspace/archive/master.zip
-unzip -o master.zip
-cp -R vaadin-idea-workspace-master/ .
-rm -rf vaadin-idea-workspace-master
-rm master.zip
